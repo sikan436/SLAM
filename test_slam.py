89c92edc25a4ce6259668f8aa49faebd45d5b7eb
sikan sikan <sikan436@gmail.com>
2024-04-29 16:39:20 +0530

    feature extractor

--- test_slam.py
+++ test_slam.py
@@ -2,31 +2,27 @@
 import time
 import cv2
 from display import Display
-from matplotlib import pyplot as plt
+from extractor import Extractor
 import numpy as np
+
 W = 1920//2
 H = 1080//2
 
 disp = Display(W, H)
-
-
-
-class FeatureExtractor(object):
-    def extract(self,img):
-          feats=cv2.goodFeaturesToTrack(np.mean(img,axis=2).astype(np.uint8),3000,qualityLevel=.01,minDistance=3)
-          # print(feats)
-
-          return feats
-
-fe=FeatureExtractor()
+fe = Extractor()
 
 def process_frame(img):
   img = cv2.resize(img, (W,H))
-  kp=fe.extract(img)
-  
-  for p in kp:
-    u,v=map(lambda x:int(round(x)),p[0])
-    cv2.circle(img,(u,v),color=(0,255,0),radius=3)
+  matches = fe.extract(img)
+
+  print("%d matches" % (len(matches)))
+
+  for pt1, pt2 in matches:
+    u1,v1 = map(lambda x: int(round(x)), pt1)
+    u2,v2 = map(lambda x: int(round(x)), pt2)
+    cv2.circle(img, (u1, v1), color=(0,255,0), radius=3)
+    cv2.line(img, (u1, v1), (u2, v2), color=(255,0,0))
+
   disp.paint(img)
 
 if __name__ == "__main__":
